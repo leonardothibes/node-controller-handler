e3c0a88bead3198ec5002927e49b0042a78ad8b9
Leonardo Thibes <leonardothibes@gmail.com>
2019-04-05 16:19:15 -0300

    testes

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const Controller = require('../lib/controller'),
+      assert     = require('unit.js');
+
+describe('Entry Point', function()
+{
+    it ('Invocation success: with callback', function(done)
+    {
+        const name     = 'John Doe';
+        const message = {
+            command: 'hello',
+            action : 'world',
+            params : {name: name},
+        };
+
+        const controller = new Controller(__dirname + '/commands');
+        controller.handle(message, function(error, data)
+        {
+            assert.bool(!error).isTrue();
+            assert.string(data).isEqualTo(`Hello ${name}!`);
+        });
+
+        done();
+    });
+});
